20b3f01a9cf5bd48c5c4707352df87aa620c6308
jonah michael <ymichael81@hotmail.com>
2023-06-01 14:22:13 +0100

    done the search bar for the index page using PG Search

--- app/models/lego.rb
+++ app/models/lego.rb
@@ -2,4 +2,11 @@ class Lego < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description_and_price_and_location,
+    against: [ :title, :description, :price, :location ],
+    using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
